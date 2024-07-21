9ed8d7faa51a9bfc343ab69d48ca7dbf17e90b4b
iuliadavid <david.iulia4500@gmail.com>
2024-07-21 19:31:47 +0300

    595, 1148, 1683 solutions

---
+++ 1148.sql
@@ -0,0 +1,4 @@
+# Article Views I
+SELECT DISTINCT author_id as id FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC
---
+++ 1683.sql
@@ -0,0 +1,3 @@
+# Invalid Tweets
+SELECT tweet_id FROM Tweets
+WHERE LENGTH(content) > 15
